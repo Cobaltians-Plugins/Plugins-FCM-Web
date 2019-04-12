894094c8708bc08af00555ea1ae69741de44d43d
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-12 15:23:35 +0200

    removing plugin options

--- cobalt.fcm.js
+++ cobalt.fcm.js
@@ -1,52 +1,15 @@
 (function(cobalt){
     var plugin = {
         'name': 'Fcm',
-        init: function (settings) {
+        init: function () {
             // Create shortcuts
             cobalt.fcm={
                 init: this.init.bind(this),
                 getToken : this.getToken.bind(this),
                 subscribeToTopic : this.subscribeToTopic.bind(this),
-                unsubscribeFromTopic : this.unsubscribeFromTopic.bind(this),
-                tokenRegister : this.tokenRegister.bind(this),
-                getTopics : this.getTopics.bind(this),
-                saveTopic : this.saveTopic.bind(this),
-                sendNotification : this.sendNotification.bind(this)
+                unsubscribeFromTopic : this.unsubscribeFromTopic.bind(this)
             };
-
-            if (settings) {
-                this.config(settings);
-            }
         },
-
-        config: function(settings){},
-
-        //Méthode générale pour envoyer les calls ajax
-        ajax:function(params, callback){
-            var url = 'http://www.build.kristal.io/' + params.url;
-            var request = new XMLHttpRequest();
-            request.open("POST", url, true);
-            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-
-            request.onreadystatechange = function () {
-                if (request.readyState != 4 || request.status != 200){
-                    
-                }
-                else{
-                    if (typeof callback == 'function'){
-                        callback(JSON.parse(request.responseText));
-                    }
-                    else{
-                        retour = request.responseText;
-                        cobalt.log('Call à ' + params.url + ' réussi : ', retour);
-                    }
-                }
-            };
-            paramsAEnvoyer = this.objectToString(params);
-            console.log('Envoi de cette chaine de paramètres : ' + paramsAEnvoyer);
-            request.send(paramsAEnvoyer);
-        },
-
         //Récupère un objet de données à envoyer, et le transforme en chaine sous la forme champ=valeur&champ2=valeur2
         objectToString:function(params){
             var str = "";
@@ -96,62 +59,9 @@
                 }
             });
         },
-
-        tokenRegister: function(data, callback){
-            data.url = 'notifications/register';
-            this.ajax(data, function(retour){
-                if (typeof callback == 'function'){
-                    callback(retour);
-                }
-                else{
-                    cobalt.log('Call tokenRegister réussi, réponse serveur : ', retour);
-                }
-            });
-        },
-
-        getTopics:function(data, callback){
-            data.url = 'notifications/topics/get';
-            this.ajax(data, function(retour){
-
-                if (typeof callback == 'function'){
-                    callback(retour.topics);
-                }
-                else{
-                    cobalt.log('Call getTopics réussi, réponse serveur : ', retour);
-                }
-            });
-        },
-
-        saveTopic:function(data, callback) {
-            data.url = 'notifications/topics/save';
-            this.ajax(data, function(retour){
-
-                if (typeof callback == 'function'){
-                    callback(retour);
-                }
-                else{
-                    cobalt.log('Call saveTopic réussi, réponse serveur : ', retour);
-                }
-            });
-        },
-
-        sendNotification:function(data, callback){
-            data.url = 'notifications/push';
-            this.ajax(data, function(retour){
-
-                if (typeof callback == 'function'){
-                    callback(retour);
-                }
-                else{
-                    cobalt.log('Call saveTopic réussi, réponse serveur : ', retour);
-                }
-            });
-        },
-
         handleEvent:function(json){
             cobalt.log(this.name, ' plugin : unknown event received :', json)
         },
-
         send:function(action, data, callback){
             cobalt.send({ type : "plugin", name : this.name, action : action, data : data }, callback);
         }
