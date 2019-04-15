bec59fc528c29f3bc0ea5077ddecf3a738aeee71
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 10:06:59 +0200

    ready to handle event

--- cobalt.fcm.js
+++ cobalt.fcm.js
@@ -1,67 +1,54 @@
-(function(cobalt){
-    var plugin = {
-        classes: {
-        	android: 'io.kristal.fcmplugin.FcmPlugin',
-			ios: 'FcmPlugin'
+(function(cobalt) {
+  var plugin = {
+    classes: {
+      android: 'io.kristal.fcmplugin.FcmPlugin',
+      ios: 'FcmPlugin'
+    },
+    init: function() {
+      cobalt.fcm = {
+        init: this.init.bind(this),
+        getToken: this.getToken.bind(this),
+        subscribeToTopic: this.subscribeToTopic.bind(this),
+        unsubscribeFromTopic: this.unsubscribeFromTopic.bind(this),
+        callbacks:{
+          onGetTokenResult: function(data){ cobalt.log('fcm plugin, getToken result', data)},
+          onSubscribeToTopicResult: function(data){ cobalt.log('fcm plugin, subscribeToTopic result', data)},
+          onUnsubscribeToTopicResult: function(data){ cobalt.log('fcm plugin, unsubscribeFromTopic result', data)},
         }
-        init: function () {
-            // Create shortcuts
-            cobalt.fcm={
-                init: this.init.bind(this),
-                getToken : this.getToken.bind(this),
-                subscribeToTopic : this.subscribeToTopic.bind(this),
-                unsubscribeFromTopic : this.unsubscribeFromTopic.bind(this)
-            };
-        },
-        //Récupère un objet de données à envoyer, et le transforme en chaine sous la forme champ=valeur&champ2=valeur2
-        objectToString:function(params){
-            var str = "";
-            for (var key in params) {
-                if (str != "") {
-                    str += "&";
-                }
-                str += key + "=" + encodeURIComponent(params[key]);
-            }
-            return str;
-        },
+      };
+    },
+    getToken: function(callback) {
+      cobalt.fcm.callbacks.onGetTokenResult = callback;
+      cobalt.plugins.send(this, "getToken", {});
+    },
+    subscribeToTopic: function(topic, callback) {
+      cobalt.fcm.callbacks.onSubscribeToTopicResult = callback;
+      cobalt.plugins.send(this, "subscribeToTopic", {'topic': topic});
+    },
 
-
-        getToken: function(callback){
-            cobalt.log("Sending event getToken to the native side")
-            cobalt.plugins.send(this, "getToken", {}, function(data){
-                if (typeof callback =="function"){
-                    callback(data);
-                }
-                else{
-                    cobalt.log('Received infos = ', data, typeof callback);
-                }
-            });
-        },
-
-        subscribeToTopic: function(topic, callback){
-            cobalt.log("Sending event subscribeToTopic to the native side")
-            var donnees = {'topic': topic};
-            cobalt.plugins.send(this, "subscribeToTopic", donnees, function(data){
-                if (typeof callback == 'function'){
-                    callback(data);
-                }
-                else{
-                    cobalt.log('Received infos = ', data, typeof callback);
-                }
-            });
-        },
-
-        unsubscribeFromTopic: function(topic, callback){
-            var donnees = {'topic': topic};
-            cobalt.plugins.send(this, "unsubscribeFromTopic", donnees, function(data){
-                if (typeof callback == 'function'){
-                    callback(data);
-                }
-                else{
-                    cobalt.log('Received infos = ', data, typeof callback);
-                }
-            });
-        }
-    };
-    cobalt.plugins.register(plugin);
+    unsubscribeFromTopic: function(topic, callback) {
+      cobalt.fcm.callbacks.onUnsubscribeToTopicResult = callback;
+      cobalt.plugins.send(this, "unsubscribeFromTopic", {'topic': topic});
+    },
+    handleEvent: function(json) {
+      switch (json && json.action) {
+        case 'getToken':
+          if (typeof cobalt.fcm.callbacks.onGetTokenResult === 'function') {
+            cobalt.fcm.callbacks.onGetTokenResult(json.data);
+          }
+          break;
+        case 'subscribeToTopic':
+          if (typeof cobalt.fcm.callbacks.onSubscribeToTopicResult === 'function') {
+            cobalt.fcm.callbacks.onSubscribeToTopicResult(json.data);
+          }
+          break;
+        case 'unsubscribeFromTopic':
+          if (typeof cobalt.fcm.callbacks.onUnsubscribeToTopicResult === 'function') {
+            cobalt.fcm.callbacks.onUnsubscribeToTopicResult(json.data);
+          }
+          break;
+      }
+    }
+  };
+  cobalt.plugins.register(plugin);
 })(cobalt || {});
